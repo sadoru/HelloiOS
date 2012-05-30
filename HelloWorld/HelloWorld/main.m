b4f2279082b810e128fa3f525676f975eed3fde2
sadoru <sadoroo@gmail.com>
2012-05-30 20:38:30 +0900

    add project
    既存projを追加してみる。

---
+++ HelloWorld/HelloWorld/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by Saitoh Sadoru on 12/05/29.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HWAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HWAppDelegate class]));
+    }
+}
